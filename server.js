38d140c72d4c5dfc99cf98af7d10062c050527fc
luisfelipe123poi <luiscastellar1505@gmail.com>
2026-08-01 22:33:57 -0500

    server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ Ofrecemos atención de alta calidad, acompañamiento continuo y facilidades de a
 const USAGE_LIMIT_FREE_TRIAL = 1;
 import cron from 'node-cron';
 import axios from 'axios';
-require('dotenv').config();
+import 'dotenv/config';
 
 // Helper para generar claves de licencia únicas (ej: PRES-A1B2-C3D4-E5F6 o FREE-A9F4B2)
 function generateLicenseKey(prefix = 'PRES') {
